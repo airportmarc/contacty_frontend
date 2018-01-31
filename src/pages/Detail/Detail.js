a064acbab72dccda8bacdd3f80ff56b59f40e8bb
airportmarc <marcwitney@live.com>
2018-01-31 12:42:56 -0500

    New interface, removing numbers

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -20,21 +20,41 @@ class Detail extends Component {
             sendTo: ''
 
         }
-        let temp = []
-        temp = mockContacts.filter((item) => {
-            if (item.pk == props.match.params.number) {
-                return true
-            }
-            return false
-        })
-        this.state.contact = temp[0]
 
         this.makeAction = this.makeAction.bind(this)
         this.updateMessage = this.updateMessage.bind(this)
         this.Delete = this.Delete.bind(this)
         this.addEvent = this.addEvent.bind(this)
+        this.deleteItem = this.deleteItem.bind(this)
     }
 
+    componentDidMount() {
+        const payload = {
+            params: {
+                limit: 15
+            }
+        }
+        ax.get(`/users/${this.props.match.params.number}`, payload )
+        .then(res => {
+            console.log(res.data)
+            this.setState({contact: res.data})
+        })
+
+    }
+
+    deleteItem(style, id) {
+        console.log(style, id)
+        const stylePlural = [style + 's'][0]
+        console.log(style, id, stylePlural)
+
+        ax.delete(`/users/${this.props.match.params.number}/${style}/${id}`).then((res) => {
+            const newList = this.state.contact.contact[stylePlural].filter( phone => phone.id !== id)
+            const TempContact = Object.assign({}, this.state.contact)
+            TempContact.contact[stylePlural] = newList
+            this.setState({contact: TempContact})
+        })
+
+    }
     addEvent(eventType, message) {
         let currentEvent = this.state.events.slice()
         const item = {
@@ -60,116 +80,89 @@ class Detail extends Component {
         console.log("Making a Call")
     }
     render() {
-
-        let closeModel = () => this.setState({isModelOpen: false})
-
-        let sendAndClose = () => {
-            const payload = {
-                actionType: this.state.actionType,
-                sendTo: this.state.sendTo,
-                message: this.state.message
-            }
-            ax.post('/makeAction', payload)
-            .then( ()=> this.setState({isModelOpen: false}))
+        let phoneList, emailList = 'No Information Availabile'
+        if(this.state.contact.contact && this.state.contact.contact.phones.length > 0) {
+            phoneList  = this.state.contact.contact.phones.map( (phone, idx) => {
+            return <li key={phone.id}>{phone.number} <i className="fa fa-minus" onClick={ () => this.deleteItem('phone', phone.id)}></i></li>
+            })
+        }
+        if (this.state.contact.contact && this.state.contact.contact.emails.length > 0) {
+            emailList  = this.state.contact.contact.emails.map( (email, idx) =>{
+                    return <li key={email.id}>{email.email} <i className="fa fa-minus"  onClick={ () => this.deleteItem('email', email.id)}></i></li>
+        })
         }
         return (
-            <div className="wrapper wrapper-content animated fadeInRight">
-                <div className="row m-b-lg m-t-lg">
-                    <div className="col-md-6">
-                        <div className="profile-image">
+        <div className="row animated fadeInRight">
+                <div className="col-md-4">
+                    <div className="ibox float-e-margins">
+                        <div className="ibox-title">
+                            <h5>Profile Detail</h5>
                         </div>
-                        <div className="profile-info">
-                            <div className="">
-                                <div>
-                                    <h2 className="no-margins">
-                                        {this.state.contact.first_name} {this.state.contact.last_name}
-                                    </h2>
-                                    <h4>Founder of Groupeq</h4>
-                                    <small>
-                                        There are many variations of passages of Lorem Ipsum available, but the majority
-                        have suffered alteration in some form Ipsum available.
-                    </small>
+                        <div>
+                            <div className="ibox-content">
+                                <h4><strong>{this.state.contact.first_name} {this.state.contact.last_name}</strong></h4>
+                                <p><i className="fa fa-map-marker"></i> {this.state.contact.organization}</p>
+                                <h5>
+                                    About me
+                                </h5>
+                                <p>
+                                    {this.state.contact.bio}
+                                </p>
+                                <div className="user-button">
+                                    <div className="row">
+                                        <div className="col-md-3">
+                                            <button type="button" className="btn btn-primary btn-sm btn-block" onClick={this.makeAction}><i className="fa fa-envelope"></i>Text</button>
+                                        </div>
+                                        <div className="col-md-3">
+                                            <button type="button" className="btn btn-default btn-sm btn-block" onClick={this.makeAction}><i className="fa fa-coffee"></i>Call</button>
+                                        </div>
+                                        <div className="col-md-3">
+                                            <button type="button" className="btn btn-default btn-sm btn-block" onClick={this.makeAction}><i className="fa fa-coffee"></i>Email</button>
+                                        </div>
+                                        <div className="col-md-3">
+                                            <button type="button" className="btn btn-default btn-sm btn-block" onClick={this.makeDelete}><i className="fa fa-coffee"></i>Delete</button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="col-md-3">
-                    <div className="row">
-                            <div className="col-xs-3">
-                                <button className="btn btn-lg btn-primary action-buttons" onClick={this.makeAction}><i className="fa fa-phone"></i></button>
-                            </div>
-                            <div className="col-xs-3">
-                                <button className="btn btn-lg btn-primary action-buttons" onClick={this.makeAction}><i className="fa fa-mobile"></i></button>
-                            </div>
-                        </div>
-                    <div className="row">
-                            <div className="col-xs-3">
-                                <button className="btn btn-lg btn-primary action-buttons" onClick={this.makeEmail}><i className="fa fa-envelope"></i></button>
-                            </div>
-                            <div className="col-xs-3">
-                                <button className="btn btn-lg btn-warn action-buttons" onClick={this.makeDelete}> <i className="fa fa-close"></i></button>
-                            </div>
                     </div>
                 </div>
+                    </div>
+                <div className="col-md-8">
+                <div className="ibox float-e-margins">
+                        <div className="ibox-title">
+                            <h5>Details</h5>
+                        </div>
+                        <div className="ibox-content">
+                        <p>Phones <i className="fa fa-plus" onClick={this.addNumber}></i></p>
+                        {phoneList}
+                        <p>Emails <i className="fa fa-plus"  onClick={this.addNumber}></i> </p>
+                        {emailList}
+                        </div>
+                </div>
 
+                    <div className="ibox float-e-margins">
+                        <div className="ibox-title">
+                            <h5>Activites</h5>
+                        </div>
+                        <div className="ibox-content">
+
+                            <div>
+                                <div id="vertical-timeline" className="vertical-container light-timeline no-margins">
+                                        {this.state.events.map((item, i) => <EventItem item={item} key={i} />)}
+
+                                </div>
+                                </div>
+
+
+                    </div>
 
                 </div>
-                <div className="row">
-
-                    <div className="col-lg-3">
-
-                        <div className="ibox">
-                            <div className="ibox-content">
-                                <h3>About {this.state.contact.first_name} {this.state.contact.last_name} </h3>
-
-                                <p className="small">
-                                   {this.state.contact.bio}
-                </p>
-
-
-                            </div>
-                        </div>
-
-
-
-                    </div>
-
-                    <div className="col-lg-5">
-
-                    </div>
-                    <div className="col-lg-4 m-b-lg">
-                        <div id="vertical-timeline" className="vertical-container light-timeline no-margins">
-                            {this.state.events.map((item, i) => <EventItem item={item} key={i} />)}
-
-                        </div>
-                    </div>
-                </div>
-                <Modal
-          show={this.state.isModelOpen}
-          onHide={closeModel}
-          aria-labelledby="ModalHeader"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id='ModalHeader'>Send a message</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-
-            <textarea value={this.state.message}  onChange={this.updateMessage}></textarea>
-          </Modal.Body>
-          <Modal.Footer>
-
-            <Modal.Dismiss className='btn btn-default'>Cancel</Modal.Dismiss>
-
-            <button className='btn btn-primary' onClick={sendAndClose}>
-              Save
-            </button>
-          </Modal.Footer>
-        </Modal>
-
-
             </div>
-        )
-    }
+            </div>
+
+        )}
 }
+
 
 export default Detail
