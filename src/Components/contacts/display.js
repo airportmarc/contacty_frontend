4569c1f1b72de68b21109ef32fbb5837b3503c21
airportmarc <marcwitney@live.com>
2018-02-22 14:14:45 -0500

    Fixing a 'cannot find number' errror

--- src/Components/contacts/display.js
+++ src/Components/contacts/display.js
@@ -70,7 +70,7 @@ class ContactsDisplay extends Component {
 
                                             <td><Link to={`/detail/${contact.id}`}>{contact.first_name} </Link></td>
                                             <td>{contact.last_name}</td>
-                                            <td>{ contact.contact ? contact.contact.phones[0].number : '-' }</td>
+                                            <td>{ contact.contact.phones[0] ? contact.contact.phones[0].number : '-' }</td>
                                             <td>{contact.email}</td>
                                         </tr>
 
